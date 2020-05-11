6fb820f9ae8a750e779fdd18723f5f2e90898efe
zara ahmadi <zatokey@gmail.com>
2020-05-11 13:32:01 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -66,10 +66,8 @@ def add_characters
 
 epic_tragedy[:additional_characters] = [
   {:name = "Prince Escalus"},
-  {:name = "Apothecary"},
+  {:name = "Apothecary"},]
 
-]
-epic_tragedy[:additional_characters][:name] = "Apothecary"
 
 
 
