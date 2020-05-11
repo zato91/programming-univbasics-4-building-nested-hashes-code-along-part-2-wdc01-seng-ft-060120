57ab02c1ccbfebeb2288fe114f28dbda506b4c9e
zara ahmadi <zatokey@gmail.com>
2020-05-11 13:22:30 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -22,6 +22,8 @@ def update_status
 
   # Write code that changes the status of both Romeo and Juliet from "alive" to "dead"
   # Code your solution here:
+  epic_tragedy[:montague]
+  epic_tragedy[:capulet]
   
 
 
